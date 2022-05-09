7bcdf2fdb9478638b490a6f79814fa834031846a
sachin-ezee <sachin.devaiya@ezeetechnosys.com>
2022-05-09 11:47:37 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="justify-center flex bg-pink-300 items-center h-screen">
+    <div class="text-4xl">Hello 👋🏼</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
